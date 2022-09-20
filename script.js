9db1dde1d0b2b3bcf4beaa6c42b13ad449097c98
alysvolatile <alice.g.cooper@gmail.com>
2022-09-20 10:15:27 -0400

    init commit and create basic counter app

---
+++ script.js
@@ -0,0 +1,27 @@
+let counter = 0;
+let counterSpan = document.getElementById('counter')
+let subtractButton = document.getElementById('subtract')
+let resetButton = document.getElementById('reset')
+let addButton = document.getElementById('add')
+
+counterSpan.innerText = `${counter}`
+
+function countUp() {
+    ++counter
+    counterSpan.innerText = `${counter}`
+    return counter
+}
+function countDown() {
+    --counter
+    counterSpan.innerText = `${counter}`
+    return counter
+}
+function reset() {
+    counter = 0
+    counterSpan.innerText = `${counter}`
+    return counter
+}
+
+addButton.addEventListener('click', countUp)
+subtractButton.addEventListener('click', countDown)
+resetButton.addEventListener('click', reset)
